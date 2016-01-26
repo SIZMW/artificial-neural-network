292ae5fe96369a3df803c4eeee4f65862dc85c5f
Daniel Beckwith <djbsnx@gmail.com>
2016-01-26 16:01:50 -0500

    Filled in Neuron class, removed shared run config

--- Neuron.py
+++ Neuron.py
@@ -1,3 +1,22 @@
+import operator
+from math import exp
+
+
+def activation_curve(x):
+    return 1 / (1 + exp(x))
+
+
 class Neuron:
-    def __init__(self):
-        pass
+    def __init__(self, num_inputs):
+        """
+        :param num_inputs: int
+        :return: Neuron
+        """
+        self.weights = [0] * num_inputs
+
+    def activate(self, inputs):
+        """
+        :param inputs: list(float)
+        :return: int
+        """
+        return round(activation_curve(sum(map(operator.mul, self.weights, inputs))))
