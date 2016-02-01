bcbb30068daf6a2f56a7ecc34820a9afdf18e2f4
Aditya Nivarthi <adityanivarthi19@gmail.com>
2016-02-01 12:39:33 -0500

    Fixed argument parsing for ann.py. No changes to neural net.

--- ann.py
+++ ann.py
@@ -32,21 +32,24 @@ def main():
     by Daniel Beckwith and Aditya Nivarthi
     for WPI CS 4341
     ''')
-    parser.add_argument('filename')
-    parser.add_argument('nodesorholdback', nargs='?', default='h')
-    parser.add_argument('value', nargs='?', type=float)
 
+    parser.add_argument('filename')
+    parser.add_argument('h', default='h')
+    parser.add_argument('hidden_node_count', type=int)
+    parser.add_argument('p', default='p')
+    parser.add_argument('percent', type=float)
     args = parser.parse_args()
+
     points, expec_output = read_data(args.filename)
 
     hold_back = 0.2
     node_count = 5
+    hold_back = args.percent
+    node_count = args.hidden_node_count
 
-    if args.nodesorholdback == 'p':
-        hold_back = args.value
-        if hold_back < 0.0 or hold_back > 1.0:
-            # Invalid percentage
-            raise NameError('Hold back percentage is invalid.')
+    if hold_back < 0.0 or hold_back > 1.0:
+        # Invalid percentage
+        raise NameError('Hold back percentage is invalid.')
 
     net = NeuralNet(2, node_count, 1)
 
